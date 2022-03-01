54f94891685a4b185925c67cc8db2fd387d14ac9
HPanTroG <nthuyen.bmth@gmail.com>
2022-03-01 21:03:10 +0100

    delete test

--- code/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('../data/nepal.csv')
-data  = data[['tweet_id', 'tweet_text', 'corrected_label', 'informative_content']]
-data.columns = ['tweet_id', 'tweet_text', 'tweet_label', 'rationale_label']
-data.to_csv("../data/nepal.csv", header = None, index=False)
